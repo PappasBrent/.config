3a8f76045441376ba70d7c9f3fa47adbf4511a8e
Brent Pappas <pappasbrent@gmail.com>
2026-04-10 12:33:25 -0400

    Trim whitespace

--- bash/setup.sh
+++ bash/setup.sh
@@ -2,11 +2,10 @@
 
 set -eu
 
-ln -sf $(realpath .bash_aliases)     ~/.bash_aliases   
+ln -sf $(realpath .bash_aliases)     ~/.bash_aliases
 ln -sf $(realpath .bash_completion)  ~/.bash_completion
 ln -sf $(realpath .bash_environment) ~/.bash_environment
-ln -sf $(realpath .bash_logout)      ~/.bash_logout    
-ln -sf $(realpath .bash_profile)     ~/.bash_profile   
-ln -sf $(realpath .bashrc)           ~/.bashrc         
+ln -sf $(realpath .bash_logout)      ~/.bash_logout
+ln -sf $(realpath .bash_profile)     ~/.bash_profile
+ln -sf $(realpath .bashrc)           ~/.bashrc
 ln -sf $(realpath .inputrc)          ~/.inputrc
-
